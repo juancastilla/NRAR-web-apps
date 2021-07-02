b8ee71495afcbdadecfb13c0f50e0d2fb8ae386b
Juan Castilla-Rho <jccastillarho@gmail.com>
2021-07-02 20:53:29 +1000

    fixing path issue for deploy

--- streamlit/NRAR_web_app.py
+++ streamlit/NRAR_web_app.py
@@ -46,9 +46,9 @@ if selection == 'Compliance Data Viz':
     
     custom_date_parser = lambda x: datetime.strptime(x, "%b-%y")
     nan_values_list = ["NaN",' ']
-    df_casedetails=pd.read_csv('../data/compliance_dataviz/Case Details.csv', na_values=nan_values_list, parse_dates=['Received Date','Finalisation Date'], date_parser=custom_date_parser, index_col=0)
-    df_enfactions_count=pd.read_csv('../data/compliance_dataviz/Enforcement actions — count.csv', na_values=nan_values_list, index_col=0)
-    df_enfactions_description=pd.read_csv('../data/compliance_dataviz/Enforcement actions — description.csv', parse_dates=['Enforcement Action Date'], date_parser=custom_date_parser, na_values=nan_values_list, index_col=0)
+    df_casedetails=pd.read_csv('data/compliance_dataviz/Case Details.csv', na_values=nan_values_list, parse_dates=['Received Date','Finalisation Date'], date_parser=custom_date_parser, index_col=0)
+    df_enfactions_count=pd.read_csv('data/compliance_dataviz/Enforcement actions — count.csv', na_values=nan_values_list, index_col=0)
+    df_enfactions_description=pd.read_csv('data/compliance_dataviz/Enforcement actions — description.csv', parse_dates=['Enforcement Action Date'], date_parser=custom_date_parser, na_values=nan_values_list, index_col=0)
     df_casedetails['Received Date'] = df_casedetails['Received Date'].dt.strftime("%b-%y")
     df_casedetails['Finalisation Date'] = df_casedetails['Finalisation Date'].dt.strftime("%b-%y")
     df_enfactions_description['Enforcement Action Date'] = df_enfactions_description['Enforcement Action Date'].dt.strftime("%b-%y")
