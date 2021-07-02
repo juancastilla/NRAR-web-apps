d246885fbaffdfe1d40c63b279dfb0c62ce8c4d7
Juan Castilla-Rho <jccastillarho@gmail.com>
2021-07-02 16:17:55 +1000

    compliance data sunburst chart now configurable via selectboxes and running with Plotly

--- streamlit/NRAR_web_app.py
+++ streamlit/NRAR_web_app.py
@@ -37,51 +37,50 @@ st.sidebar.title('About')
 
 st.sidebar.info("NRAR's interdisciplinary behavioural and cost optimisation model: A collection of Streamlit web apps for data analysis, data visualisation and exploratory modelling")
 
+## HELPER FUNCTIONS ##
+
 
 if selection == 'Compliance Data Viz':
+    
+    st.markdown('# Compliance Data — Sunburst Plot ')
+    
+    custom_date_parser = lambda x: datetime.strptime(x, "%b-%y")
+    nan_values_list = ["NaN",' ']
+    df_casedetails=pd.read_csv('../data/compliance_dataviz/Case Details.csv', na_values=nan_values_list, parse_dates=['Received Date','Finalisation Date'], date_parser=custom_date_parser, index_col=0)
+    df_enfactions_count=pd.read_csv('../data/compliance_dataviz/Enforcement actions — count.csv', na_values=nan_values_list, index_col=0)
+    df_enfactions_description=pd.read_csv('../data/compliance_dataviz/Enforcement actions — description.csv', parse_dates=['Enforcement Action Date'], date_parser=custom_date_parser, na_values=nan_values_list, index_col=0)
+    df_casedetails['Received Date'] = df_casedetails['Received Date'].dt.strftime("%b-%y")
+    df_casedetails['Finalisation Date'] = df_casedetails['Finalisation Date'].dt.strftime("%b-%y")
+    df_enfactions_description['Enforcement Action Date'] = df_enfactions_description['Enforcement Action Date'].dt.strftime("%b-%y")
+    df_casedetails.sort_values('Received Date', inplace=True)
+        
+    
+    def plot_sunburst():
+        path_title = ', '.join(level_list)
+        df = df_casedetails.groupby(level_list).count().reset_index()
+        df['count'] = df.iloc[:,-1:]
+        fig = px.sunburst(df, path=level_list, values='count', width=900, height=900)
+        fig.update_layout(title=path_title)
+        st.plotly_chart(fig)
+    
+    with st.form(key='columns_in_form'):
+        st.markdown('### **Select ordering of variables**')
+        cols = st.beta_columns(5)
+        level_1 = cols[0].selectbox('Level 1', ['Industry', 'LGA', 'WSP', 'Received Date', 'Finalisation Date', 'Offences','Finalisation Reason', 'none'], key='a', index=5)
+        level_2 = cols[1].selectbox('Level 2', ['Industry', 'LGA', 'WSP', 'Received Date', 'Finalisation Date', 'Offences','Finalisation Reason', 'none'], key='b', index=6)
+        level_3 = cols[2].selectbox('Level 3', ['Industry', 'LGA', 'WSP', 'Received Date', 'Finalisation Date', 'Offences','Finalisation Reason', 'none'], key='c', index=1)
+        level_4 = cols[3].selectbox('Level 4', ['Industry', 'LGA', 'WSP', 'Received Date', 'Finalisation Date', 'Offences','Finalisation Reason', 'none'], key='d', index=7)
+        level_5 = cols[4].selectbox('Level 5', ['Industry', 'LGA', 'WSP', 'Received Date', 'Finalisation Date', 'Offences','Finalisation Reason', 'none'], key='e', index=7)
+        st.write('Press submit to update the plot')
+        submitted = st.form_submit_button('Submit')
+        
+        if submitted:
+            level_list = [level_1, level_2, level_3, level_4, level_5]
+            # items to be removed
+            unwanted = {'none'}
+            level_list = [ele for ele in level_list if ele not in unwanted]
+            #level_list
+            plot_sunburst()
+            
 
-    if platform.system() == 'Darwin':
-        with open('../html/Case_Details_Sunburst.html', 'r') as f:
-            html_string = f.read()
-        components.html(html_string, width=1500, height=1500)
-    # else:
-    #     markdown = read_markdown_file(os.path.join(stdir,'submit_page.md'))
-
-    # st.markdown(markdown, unsafe_allow_html=True)
-
-    #components.iframe("https://docs.google.com/forms/d/e/1FAIpQLSeOgQtYLJALacZQfwF2Nb5RMWOqg_ODVyyEXoStBKHekfg66w/viewform?usp=sf_link", height=1500, scrolling=True)
-
-# if selection == 'Trade-Off Analysis':
-
-    # if platform.system() == 'Darwin':
-    #     markdown = read_markdown_file('about_page.md')
-    # else:
-    #     markdown = read_markdown_file(os.path.join(stdir,'about_page.md'))
-
-    # st.markdown(markdown, unsafe_allow_html=True)
-
-# Main app page — groundwater model display and search map
-# NOTE: this could be refactored into a separate .py script and import on app start
-
-# if selection == 'Find Models':
-
-    # st.title('GroMoPo — Groundwater Model Portal')
-
-    # st.write("Sharing groundwater model data, knowledge and insights more easily through a portal of regional and global numerical groundwater models. The first priority is archiving existing models, but the repository could eventually archive model input and scripts for translating commonly used geospatial datasets into model inputs.")
-
-    # map = folium.Map(zoom_start=3, crs='EPSG{}'.format(epsg),min_zoom=3,max_bounds=True)
-    # folium.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', attr='x',name='OpenTopoMap').add_to(map)
-    # folium.TileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',name='ArcWorldImagery', attr='x').add_to(map)
-
-
-    # rgroup, marker_cluster, mlayer = plot_map(all_gdf,img,popup=popup)
-
-    # rgroup.add_to(map)
-    # marker_cluster.add_to(map)
-    # mlayer.add_to(map)
-
-    # map.add_child(folium.LayerControl())
-
-    # Fullscreen().add_to(map)
-
-    # folium_static(map, height=700, width=1400)
+        
